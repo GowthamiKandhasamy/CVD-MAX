836f3972bf3c2466d988d0777d63ef3e136e42aa
Gowthami Kandhasamy <gowthamikandhasamy1@gmail.com>
2026-03-01 16:34:38 +0530

    Review 2: Setup dataset derivation pipeline structure

---
+++ src/dataset/1_config.py
@@ -0,0 +1,11 @@
+RAW_DATA_PATH = "data/raw/diversevul.csv"
+
+INTERIM_DIR = "data/interim/"
+PROCESSED_DIR = "data/processed/"
+
+CNT_MIN_01 = 1
+MAX_RATIO_0_1 = 3
+
+TRAIN_RATIO = 0.8
+VAL_RATIO = 0.1
+TEST_RATIO = 0.1
